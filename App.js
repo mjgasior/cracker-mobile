3612ab2aa61583df0a31697ef7b75f60fc8beb17
Michal J. Gasior <michaljgasior@gmail.com>
2020-04-04 19:27:15 +0200

    Extract initialization

--- App.js
+++ App.js
@@ -1,52 +1,21 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { SplashScreen } from "expo";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import useLinking from "./navigation/useLinking";
 import { useFirebase } from "./useFirebase";
+import { useInitialize } from "./useInitialize";
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
   useFirebase();
-
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-  const [initialNavigationState, setInitialNavigationState] = useState();
-  const containerRef = useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-          ...Ionicons.font,
-          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
+  const {
+    containerRef,
+    isLoadingComplete,
+    initialNavigationState
+  } = useInitialize();
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return null;
---
+++ useInitialize.js
@@ -0,0 +1,46 @@
+import { useState, useRef, useEffect } from "react";
+import { SplashScreen } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import useLinking from "./navigation/useLinking";
+
+export const useInitialize = () => {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [initialNavigationState, setInitialNavigationState] = useState();
+  const containerRef = useRef();
+  const { getInitialState } = useLinking(containerRef);
+
+  // Load any resources or data that we need prior to rendering the app
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load our initial navigation state
+        setInitialNavigationState(await getInitialState());
+
+        // Load fonts
+        await Font.loadAsync({
+          Roboto: require("native-base/Fonts/Roboto.ttf"),
+          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+          ...Ionicons.font,
+          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return {
+    isLoadingComplete,
+    initialNavigationState,
+    containerRef
+  };
+};
