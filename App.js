1c2b74e97f67fff354b974d06095e36967b101df
Michał J. Gąsior <michaljgasior@gmail.com>
2020-05-30 13:49:26 +0200

    Added setInterval to monitor the position  (#14)

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
-import useCachedResources from "./hooks/useCachedResources";
+import { useCachedResources } from "./+hooks/useCachedResources";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import LinkingConfiguration from "./navigation/LinkingConfiguration";
 
