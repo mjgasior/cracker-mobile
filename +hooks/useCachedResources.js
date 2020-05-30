e76e343710c13a499c1d3114335bd7c647370def
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-30 13:39:00 +0200

    Refactor

--- +hooks/useCachedResources.js
+++ +hooks/useCachedResources.js
@@ -1,9 +1,9 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
 
-export default function useCachedResources() {
+export const useCachedResources = () => {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
 
   // Load any resources or data that we need prior to rendering the app
@@ -15,7 +15,7 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
@@ -30,4 +30,4 @@ export default function useCachedResources() {
   }, []);
 
   return isLoadingComplete;
-}
+};
