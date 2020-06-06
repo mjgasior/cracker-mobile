1314679c8e41cbc04069c1af2a7df08c26a7a6bf
Michal J. Gasior <michaljgasior@gmail.com>
2020-06-06 13:02:03 +0200

    Cleanup

--- screens/+components/Arrow.jsx
+++ screens/+components/Arrow.jsx
@@ -3,15 +3,10 @@ import { View, Text, Animated } from "react-native";
 
 export const Arrow = ({ degree }) => {
   const [fadeAnim] = useState(new Animated.Value(0));
-  const [rotateAnim] = useState(new Animated.Value(0));
 
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
-      duration: 10000,
-    }).start();
-    Animated.timing(rotateAnim, {
-      toValue: Math.PI / 2,
       duration: 10000,
     }).start();
   }, []);
@@ -21,7 +16,6 @@ export const Arrow = ({ degree }) => {
       style={{
         opacity: fadeAnim, // Bind opacity to animated value
         transform: [
-          { scale: 1 },
           { rotateZ: degree },
           { perspective: 1000 }, // without this line this Animation will not render on Android while working fine on iOS
         ],
