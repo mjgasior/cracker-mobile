5b7e56257215dfa4911b684408c06b5b2be8a394
Michal J. Gasior <michaljgasior@gmail.com>
2020-06-06 12:34:37 +0200

    Animate the sign

--- screens/+components/Arrow.jsx
+++ screens/+components/Arrow.jsx
@@ -3,10 +3,15 @@ import { View, Text, Animated } from "react-native";
 
 export const Arrow = () => {
   const [fadeAnim] = useState(new Animated.Value(0));
+  const [rotateAnim] = useState(new Animated.Value(0));
 
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
+      duration: 10000,
+    }).start();
+    Animated.timing(rotateAnim, {
+      toValue: Math.PI / 2,
       duration: 10000,
     }).start();
   }, []);
@@ -15,6 +20,11 @@ export const Arrow = () => {
     <Animated.View // Special animatable View
       style={{
         opacity: fadeAnim, // Bind opacity to animated value
+        transform: [
+          { scale: 1 },
+          { rotateZ: rotateAnim },
+          { perspective: 1000 }, // without this line this Animation will not render on Android while working fine on iOS
+        ],
       }}
     >
       <View>
