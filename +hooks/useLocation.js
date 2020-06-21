a52fc74c0bfc131c5179d9a8f4bd62516409654e
Michal J. Gasior <michaljgasior@gmail.com>
2020-06-21 14:57:45 +0200

    Details with angles

---
+++ +hooks/useLocation.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import * as Permissions from "expo-permissions";
+import * as Location from "expo-location";
+
+const PERMISSIONS = {
+  GRANTED: "granted",
+};
+
+export const useLocation = () => {
+  const [location, setLocation] = useState();
+  const [subscription, setSubscription] = useState();
+
+  useEffect(() => {
+    async function setLocationWithPerms() {
+      try {
+        const permissions = await Permissions.askAsync(Permissions.LOCATION);
+
+        if (permissions.status === PERMISSIONS.GRANTED) {
+          const subscriptionObject = await Location.watchPositionAsync(
+            {
+              accuracy: Location.Accuracy.Highest,
+              enableHighAccuracy: true,
+              distanceInterval: 0,
+              timeInterval: 1000,
+            },
+            async (locationData) => {
+              const headingData = await Location.getHeadingAsync();
+              locationData.coords.heading = headingData.trueHeading;
+              setLocation(locationData);
+            }
+          );
+          setSubscription(subscriptionObject);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    setLocationWithPerms();
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, []);
+
+  return location;
+};
--- details/DetailsScreen.jsx
+++ details/DetailsScreen.jsx
@@ -1,17 +1,56 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Button } from "react-native";
 import { Arrow } from "../+components/Arrow";
+import {
+  formatToDegrees,
+  formatDistance,
+  getDistanceFromLatLonInKm,
+  getAngleInRadians,
+  rad2deg,
+} from "../+utils/distanceCalculator";
+import { StyledText } from "../+components/StyledText";
+import { useLocation } from "../+hooks/useLocation";
+import styled from "styled-components/native";
+
+const TextBlock = styled.Text`
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
 
 export const DetailsScreen = ({ navigation, route }) => {
-  const { description, name } = route.params;
+  const location = useLocation();
+  const { latitude, longitude, description, name } = route.params;
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>{name}</Text>
-      <Arrow radians={0} />
-      <Text>{description.polish}</Text>
-      <Text>{description.english}</Text>
-      <Button title="Go back" onPress={navigation.goBack} />
-    </View>
-  );
+  if (location) {
+    const distance = getDistanceFromLatLonInKm(
+      location.coords.latitude,
+      location.coords.longitude,
+      latitude,
+      longitude
+    );
+
+    const angle = getAngleInRadians(
+      latitude,
+      longitude,
+      location.coords.latitude,
+      location.coords.longitude
+    );
+
+    const formattedDistance = formatDistance(distance);
+    const transposedAngle = angle + rad2deg(location.coords.heading);
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <TextBlock>{name}</TextBlock>
+        <Arrow radians={angle} />
+        <StyledText>
+          This marker is {formattedDistance} away ({formatToDegrees(angle)}).
+        </StyledText>
+        <Arrow radians={transposedAngle} />
+        <TextBlock>{description.polish}</TextBlock>
+        <TextBlock>{description.english}</TextBlock>
+        <Button title="Go back" onPress={navigation.goBack} />
+      </View>
+    );
+  }
+  return null;
 };
