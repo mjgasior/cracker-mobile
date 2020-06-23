ce70d0afbe8394815d60e6ce69cc1a2099c592c6
Michal J. Gasior <michaljgasior@gmail.com>
2020-06-23 22:49:42 +0200

    Add map

--- details/DetailsScreen.jsx
+++ details/DetailsScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import { Arrow } from "../+components/Arrow";
 import {
   formatToDegrees,
@@ -12,13 +12,15 @@ import { StyledText } from "../+components/StyledText";
 import { useLocation } from "../+hooks/useLocation";
 import styled from "styled-components/native";
 
+import MapView from "react-native-maps";
+
 const TextBlock = styled.Text`
   padding: 10px;
 `;
 
 export const DetailsScreen = ({ route }) => {
   const location = useLocation();
-  const { latitude, longitude, description, name } = route.params;
+  const { latitude, longitude, description } = route.params;
 
   if (location) {
     const distance = getDistanceFromLatLonInKm(
@@ -46,8 +48,24 @@ export const DetailsScreen = ({ route }) => {
         <Arrow radians={transposedAngle} />
         <TextBlock>{description.polish}</TextBlock>
         <TextBlock>{description.english}</TextBlock>
+        <View style={styles.container}>
+          <MapView style={styles.mapStyle} />
+        </View>
       </View>
     );
   }
   return null;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
