20afc83032fa96df465a9f8f6a7b14698663f677
Michal J. Gasior <michaljgasior@gmail.com>
2020-06-27 17:59:11 +0200

    Make function explicit

--- +utils/distanceCalculator.js
+++ +utils/distanceCalculator.js
@@ -1,30 +1,30 @@
 export function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1); // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
+  const earthRadiusInKm = 6371;
+  const deltaLatitude = degreesToRadians(lat2 - lat1);
+  const deltaLongitude = degreesToRadians(lon2 - lon1);
 
   const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.sin(deltaLatitude / 2) * Math.sin(deltaLatitude / 2) +
+    Math.cos(degreesToRadians(lat1)) *
+      Math.cos(degreesToRadians(lat2)) *
+      Math.sin(deltaLongitude / 2) *
+      Math.sin(deltaLongitude / 2);
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  const d = R * c; // Distance in km
+  const distanceInKm = earthRadiusInKm * c;
 
-  return d;
+  return distanceInKm;
 }
 
-export function deg2rad(deg) {
+function degreesToRadians(deg) {
   return deg * (Math.PI / 180);
 }
 
-export const formatDistance = (distance) => {
+export function formatDistance(distance) {
   if (distance < 1) {
     const meters = Math.ceil(distance * 1000);
     return `${meters} m`;
   }
   return `${distance.toPrecision(3)} km`;
-};
+}
