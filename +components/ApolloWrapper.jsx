45ef5cf93fcfc6ad08b4b8359be47de0470fb6ca
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-04 22:26:54 +0100

    Wrap requests

--- +components/ApolloWrapper.jsx
+++ +components/ApolloWrapper.jsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Constants from "expo-constants";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { useAuthorizationContext } from "../+hooks/useAuthorizationContext";
 
 export const ApolloWrapper = ({ children }) => {
+  const [client, setClient] = useState();
   const { auth, setIsAuthorized } = useAuthorizationContext();
 
-  console.log(auth);
+  useEffect(() => {
+    const apolloClient = new ApolloClient({
+      uri: `https://${Constants.manifest.extra.apiAddress}/api`,
+      cache: new InMemoryCache(),
+      request: (operation) => {
+        operation.setContext((context) => ({
+          headers: {
+            ...context.headers,
+            authorization: `Bearer ${auth}`,
+          },
+        }));
+      },
+    });
 
-  const client = new ApolloClient({
-    uri: `https://${Constants.manifest.extra.apiAddress}/api`,
-    cache: new InMemoryCache(),
-    request: (operation) => {
-      if (auth) {
-        setIsAuthorized(true);
-      }
+    setClient(apolloClient);
 
-      operation.setContext((context) => ({
-        headers: {
-          ...context.headers,
-          authorization: `Bearer ${auth}`,
-        },
-      }));
-    },
-  });
+    if (auth) {
+      setIsAuthorized(true);
+    }
+  }, [setClient, auth, setIsAuthorized]);
 
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  if (client) {
+    console.log(auth);
+
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  } else {
+    return null;
+  }
 };
